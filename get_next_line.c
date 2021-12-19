82a2b1f8e85115965852193e257bbe45d048d37b
Yebin Kim <yebkim@c10r4s10.42seoul.kr>
2021-12-19 17:20:43 +0900

    first commit

---
+++ get_next_line.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yebkim <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/19 13:27:49 by yebkim            #+#    #+#             */
+/*   Updated: 2021/12/19 17:02:53 by yebkim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include	"get_next_line.h"
+#include	<unistd.h>
+
+char	*ft_get_line(char *buffer)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	if (!buffer[i])
+		return (NULL);
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	str = (char *)malloc(sizeof(char) * (i + 2));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+	{
+		str[i] = buffer[i];
+		i++;
+	}
+	if (buffer[i] == '\n')
+	{
+		str[i] = buffer[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_new_buffer(char *buffer)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (!buffer[i])
+	{
+		free(buffer);
+		return (NULL);
+	}
+	str = (char *)malloc(sizeof(char) * (ft_strlen(buffer) - i + 1));
+	if (!str)
+		return (NULL);
+	i++;
+	j = 0;
+	while (buffer[i])
+		str[j++] = buffer[i++];
+	str[j] = '\0';
+	free(buffer);
+	return (str);
+}
+
+char	*ft_read_buffer(int fd, char *buffer)
+{
+	char	*buff;
+	int		bytes;
+
+	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buff)
+		return (NULL);
+	bytes = 1;
+	while (!ft_strchr(buffer, '\n') && bytes != 0)
+	{
+		bytes = read(fd, buff, BUFFER_SIZE);
+		if (bytes == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+		buff[bytes] = '\0';
+		buffer = ft_strjoin(buffer, buff);
+	}
+	free(buff);
+	return (buffer);
+}
+
+char	*get_next_line(int fd)
+{
+	char			*line;
+	static char		*buffer[4096];
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	buffer[fd] = ft_read_buffer(fd, buffer[fd]);
+	if (!buffer[fd])
+		return (NULL);
+	line = ft_get_line(buffer[fd]);
+	buffer[fd] = ft_new_buffer(buffer[fd]);
+	return (line);
+}
---
+++ get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yebkim <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/19 13:31:02 by yebkim            #+#    #+#             */
+/*   Updated: 2021/12/19 17:05:00 by yebkim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_read_buffer(int fd, char *buffer);
+char	*ft_strchr(char *s, int c);
+size_t	ft_strlen(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_get_line(char *buffer);
+char	*ft_new_buffer(char *buffer);
+
+#endif
